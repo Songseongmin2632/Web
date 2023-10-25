4ee85672ada7b1c7939523417b39d925ddc505fe
Songseongmin2632 <skj041013@naver.com>
2023-10-25 09:21:24 +0900

    "Web: c231025"

---
+++ c231012Project/src/c231024/main/java/com/classJava/user/service/UserService.java
@@ -0,0 +1,8 @@
+package c231024.main.java.com.classJava.user.service;
+
+import c231024.main.java.com.classJava.user.domain.User;
+
+public interface UserService {
+	public void add(User user);
+	public boolean isLogIn(int id);
+}
